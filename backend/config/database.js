4660db75fc5695768b178cdab3602f26208c6f2d
avka7542 <127684706+avka7542@users.noreply.github.com>
2023-10-30 17:06:16 +0200

    a

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connection = async() =>{
 
-    let url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/master";
+    let url = process.env.MONGO_URI 
 
     try {
         
